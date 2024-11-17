8315d16f75fd75c1bd442e3e47f4d6e5990f18b8
kkanden <mroskarez@gmail.com>
2024-11-17 15:28:33 +0100

    restore

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -73,7 +73,6 @@ require("mason").setup({})
 local ensure_installed = vim.tbl_keys(servers or {})
 vim.list_extend(ensure_installed, {
 	"stylua",
-	"styler",
 })
 require("mason-tool-installer").setup({
 	ensure_installed = ensure_installed,

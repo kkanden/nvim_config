cf7dc8543638707d6febe046692aaff47b0435f1
Niziol <oskar.niziol@axaxl.com>
2024-11-18 10:51:48 +0100

    axa configuration

--- after/plugin/rnvim.lua
+++ after/plugin/rnvim.lua
@@ -1,8 +1,18 @@
 require("r").setup({
 	hook = {
-		vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gn", "<Plug>RNextRChunk zz", {}),
-		vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gN", "<Plug>RPreviousRChunk zz", {}),
+		on_filetype = function()
+			vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gn", "<Plug>RNextRChunk zz", {})
+			vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gN", "<Plug>RPreviousRChunk zz", {})
+
+			vim.api.nvim_buf_set_keymap(
+				0,
+				"n",
+				"<localleader><F5>",
+				"<Cmd>lua require('r.send').cmd('shiny::runApp()')<CR>",
+				{}
+			)
+		end,
 	},
 	auto_start = "on startup",
-	pipe_keymap = "<C-g>",
+	pipe_keymap = "<localleader>,",
 })

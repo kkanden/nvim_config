9b31f106d08c701bd928359a868e7d44191a4e3c
kkanden <mroskarez@gmail.com>
2024-11-19 23:49:47 +0100

    change some mappings, enable telescope grep

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -13,9 +13,9 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-n>"] = cmp.mapping.select_next_item(),
 		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<C-b>"] = cmp.mapping.confirm({ select = true }),
 		-- Scroll the documentation window [b]ack / [f]orward
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-s>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete({}),
 	}),
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,20 +1,22 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Telescope git files" })
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "Telescope grep" })
 
 -- Shortcut for searching your Neovim configuration files
-vim.keymap.set('n', '<leader>pn', function()
-    builtin.find_files { cwd = vim.fn.stdpath 'config' }
-end, { desc = '[S]earch [N]eovim files' })
+vim.keymap.set("n", "<leader>pn", function()
+	builtin.find_files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "[S]earch [N]eovim files" })
 
 -- Slightly advanced example of overriding default behavior and theme
-vim.keymap.set('n', '<leader>/', function()
-    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-    })
-end, { desc = '[/] Fuzzily search in current buffer' })
+vim.keymap.set("n", "<leader>/", function()
+	-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		winblend = 0,
+		previewer = false,
+		layout_config = {
+			width = 0.6,
+			height = 0.6,
+		},
+	}))
+end, { desc = "[/] Fuzzily search in current buffer" })

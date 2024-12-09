6bc3d9bdc584bad84d5399bf2253b1625205ad86
Niziol <oskar.niziol@axaxl.com>
2024-12-09 13:58:23 +0100

    adjust undotree command based on operating system

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -2,3 +2,8 @@ vim.keymap.set("n", "<leader>u", function()
 	vim.cmd.UndotreeToggle()
 	vim.cmd("wincmd t")
 end)
+
+-- if on windows, use different command
+if package.config:sub(1, 1) == "\\" then
+	vim.g.undotree_DiffCommand = "FC"
+end

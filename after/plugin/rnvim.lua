eda5cea2ea9964fccb9a0ecd633a2bb65749a383
oliwka <mroskarez@gmail.com>
2024-12-09 20:05:49 +0100

    change rnvim mapping to fix :RMapsDesc command

--- after/plugin/rnvim.lua
+++ after/plugin/rnvim.lua
@@ -1,8 +1,8 @@
 require("r").setup({
 	hook = {
 		on_filetype = function()
-			vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gn", "<Plug>RNextRChunk zz", {})
-			vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gN", "<Plug>RPreviousRChunk zz", {})
+			vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gn", "<Cmd>lua require('r.rnw').next_chunk()", {})
+			vim.api.nvim_buf_set_keymap(0, "n", "<localleader>gN", "<Cmd>lua require('r.rnw').previous_chunk()", {})
 
 			-- start shiny app
 			vim.api.nvim_buf_set_keymap(
